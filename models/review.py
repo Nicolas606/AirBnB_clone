265720b885fe948eb058d729d9eff5c40e2a7136
Nicolas <castillonicolas606@gmail.com>
2021-07-02 19:58:16 +0000

    Add the public class attributes in the comments

--- models/review.py
+++ models/review.py
@@ -7,7 +7,12 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review representation"""
+    """Review class
+        Public class attributes:
+            - place_id: string - empty string: it will be the Place.id
+            - user_id: string - empty string: it will be the User.id
+            - text: string - empty string
+    """
 
     place_id = ""
     user_id = ""
