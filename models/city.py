a577e44a71854802b2f86c1a90de5abfa47fe49f
Nicolas <castillonicolas606@gmail.com>
2021-07-02 19:57:29 +0000

    Add the public class attributes in the comments

--- models/city.py
+++ models/city.py
@@ -7,7 +7,11 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """City representation"""
+    """City class
+        Public class attributes:
+            - state_id: string - empty string: it will be the State.id
+            - name: string - empty string
+    """
 
     state_id = ""
     name = ""
