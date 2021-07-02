6619dd6aacf497db6f1269d8560c1a6fda07e2af
Nicolas <castillonicolas606@gmail.com>
2021-07-02 19:59:02 +0000

    Add the public class attributes in the comments

--- models/user.py
+++ models/user.py
@@ -7,7 +7,13 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User representation"""
+    """User class
+        Public class attributes:
+            - email: string - empty string
+            - password: string - empty string
+            - first_name: string - empty string
+            - last_name: string - empty string
+    """
 
     email = ""
     password = ""
